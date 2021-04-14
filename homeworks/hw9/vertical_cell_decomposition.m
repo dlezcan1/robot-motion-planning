21f3f07c4246837c8f191979c02f1da4ee4a882d
Dimitri Lezcano <dlezcan1@jhu.edu>
2021-04-14 01:20:55 -0400

    HW9: Added base function and eval function

---
+++ homeworks/hw9/vertical_cell_decomposition.m
@@ -0,0 +1,19 @@
+%% vertical_cell_decomposition.m
+%
+% this is a function file to create the vertical cell decomposition
+% of a 2D rigid-body robot moving in the plane
+%
+% - written by: Dimitri Lezcano
+
+function [V, E, n_init, n_goal, G] = vertical_cell_decomposition(qI, qG, CB, bounds)
+	%% Arguments Block
+	arguments
+		qI (2,1);
+		qG (2,1);
+		CB cell;
+		bounds (2,4);
+	end
+	
+	%% 
+
+end
